b425a6712887a101503611e0c9d0caa26983b3c3
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-09 19:08:12 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json()); //creates the req.body object
+
+app.use(favicon(path.join(__dirname, "build", "favicon.ico"))); // mern-infrasctructure
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
