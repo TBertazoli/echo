7c5e5e4197df51d4c36d818c7220a14ca8b296bb
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-15 18:01:32 -0500

    added user routes and token middleware on server js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { getUser } from "../../utilities/users-service";
 import "./App.css";
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import NavBar from "../../components/NavBar/NavBar";
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState(getUser());
+
   return (
     <main className="App">
       {user ? (
