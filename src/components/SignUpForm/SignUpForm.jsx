b172890c7c07c105ec8a3af0ad33ea99daeda715
miguelcoria94 <miguelcoria94@gmail.com>
2024-05-15 22:40:23 -0500

    convert signup from class to function component

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,81 +1,79 @@
-import { Component } from "react";
+import { useState } from "react";
 import { signUp } from "../../utilities/users-service";
 import { useSpring, animated } from "@react-spring/web";
 
-export default class SignUpForm extends Component {
-  state = {
+export default function SignUpForm({ setUser }) {
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
     confirm: "",
     error: "",
-  };
+  });
 
-  handleChange = (evt) => {
-    this.setState({
+  const handleChange = (evt) => {
+    setFormData({
+      ...formData,
       [evt.target.name]: evt.target.value,
       error: "",
     });
   };
 
-  handleSubmit = async (evt) => {
+  const handleSubmit = async (evt) => {
     evt.preventDefault();
     try {
-      const { name, email, password } = this.state;
-      const formData = { name, email, password };
-
-      const user = await signUp(formData);
-      this.props.setUser(user);
+      const { name, email, password } = formData;
+      const user = await signUp({ name, email, password });
+      setUser(user);
     } catch {
-      this.setState({ error: "Sign Up Failed - Try Again" });
+      setFormData({ ...formData, error: "Sign Up Failed - Try Again" });
     }
   };
 
-  render() {
-    const disable = this.state.password !== this.state.confirm;
-    return (
-      <div>
-        <div className="form-container">
-          <form autoComplete="off" onSubmit={this.handleSubmit}>
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-            <label>Confirm</label>
-            <input
-              type="password"
-              name="confirm"
-              value={this.state.confirm}
-              onChange={this.handleChange}
-              required
-            />
-            <button type="submit" disabled={disable}>
-              SIGN UP
-            </button>
-          </form>
-        </div>
-        <p className="error-message">&nbsp;{this.state.error}</p>
+  const disable = formData.password !== formData.confirm;
+
+  return (
+    <div>
+      <div className="form-container">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <label>Confirm</label>
+          <input
+            type="password"
+            name="confirm"
+            value={formData.confirm}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit" disabled={disable}>
+            SIGN UP
+          </button>
+        </form>
       </div>
-    );
-  }
+      <p className="error-message">&nbsp;{formData.error}</p>
+    </div>
+  );
 }
