d6bb5eca2dd7cac88da79c0a198be6e2f23f89c1
miguelcoria94 <miguelcoria94@gmail.com>
2024-05-20 17:10:31 -0500

    wip: events list

--- src/components/Home/HomeLayout.jsx
+++ src/components/Home/HomeLayout.jsx
@@ -1,7 +1,22 @@
 import EventsList from "../Home/EventsList";
 import EventsMap from "../Home/EventsMap";
+import * as Reports from "../../utilities/reports-service";
+import { useState, useEffect } from "react";
 
-export default function HomeLayout({ longitude, latitude}) {
+export default function HomeLayout({ longitude, latitude }) {
+  const [reports, setReports] = useState([]);
+
+
+  useEffect(() => {
+    async function getReports() {
+      const reports = await Reports.getAllReports();
+      console.log(reports);
+      setReports(reports);
+    }
+    getReports();
+  }, []);
+
+  console.log(reports);
   return (
     <div className="grid grid-cols-12 h-full">
       <EventsList />
