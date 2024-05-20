3fdec7bcdd1dd017a059666a4eada391e7332fbe
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-20 14:05:51 -0500

    user routes tested and working

--- routes/api/reports.js
+++ routes/api/reports.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const reportsCtrl = require("../../controllers/api/reports");
-const ensureLoggedIn = require("../../config/ensureLoggedIn");
 
 // routes user specific
-router.post("/account", reportsCtrl.create); // create a report
-router.get("/account", reportsCtrl.show); //to show only user reports
-router.get("/account/:id", reportsCtrl.showOne); //to show only users report by id
-router.put("/account/:id", reportsCtrl.update); //
-router.delete("/account/:id", reportsCtrl.delete);
+router.post("/", reportsCtrl.create); // create a report
+router.get("/", reportsCtrl.show); //to show only user reports
+router.get("/:id", reportsCtrl.showOne); //to show only users report by id
+router.put("/:id", reportsCtrl.update); //
+router.delete("/:id", reportsCtrl.delete);
 
 module.exports = router;
