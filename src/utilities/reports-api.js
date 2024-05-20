2ee86efeadcd6291a89049c9be419e14899d4ca2
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-19 19:11:33 -0500

    added reports api for front end request

--- src/utilities/reports-api.js
+++ src/utilities/reports-api.js
@@ -4,3 +4,19 @@ import BASE_URL from "/api/reports";
 export async function create(report) {
   return sendRequest(`${BASE_URL}`, "POST", report);
 }
+
+export async function getAllReports() {
+  return sendRequest(`${BASE_URL}`);
+}
+
+export async function getOneReport(id) {
+  return sendRequest(`${BASE_URL}/${id}`);
+}
+
+export async function editReport(id, report) {
+  return sendRequest(`${BASE_URL}/${id}`, "PUT", report);
+}
+
+export async function deleteReport(id) {
+  return sendRequest(`${BASE_URL}/${id}`, "DELETE");
+}
