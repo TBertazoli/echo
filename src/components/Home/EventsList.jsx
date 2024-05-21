9ec4656c96021edd9d8b9459d6d906b81011d54e
miguelcoria94 <miguelcoria94@gmail.com>
2024-05-21 18:09:30 -0500

    add scroll indicator

--- src/components/Home/EventsList.jsx
+++ src/components/Home/EventsList.jsx
@@ -15,20 +15,25 @@ export default function EventsList({
     // test
   }
   return (
-    <div className="col-span-4 relative bg-zinc-900 border-r border-zinc-800 overflow-scroll shadow-2xl">
-      <div className="sticky top-0 flex justify-between bg-zinc-900 ">
-        <h1 className="text-white text-2xl font-semibold p-4 sticky top-0 ">
-          Events
-        </h1>
+    <div className="col-span-4 relative bg-zinc-900 border-r border-zinc-800 overflow-scroll ">
+      <div>
+        <div className="sticky top-0 flex justify-between bg-zinc-900">
+          <h1 className="text-white text-2xl font-semibold p-4 sticky top-0 ">
+            Events
+          </h1>
 
-        {/* create events buttons */}
-        <div className="flex items-center p-4">
-          <Link to="/create-event">
-            <button className="rounded-lg px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] text-base/6 placeholder:text-zinc-500 sm:text-sm/6 text-white border bg-blue-500 border-blue-600 data-[hover]:border-blue-700 bg-blue hover:bg-blue-700 focus:outline-none cursor-pointer">
-              Create Event
-            </button>
-          </Link>
+          {/* create events buttons */}
+          <div className="flex items-center p-4 pb-2">
+            <Link to="/create-event">
+              <button className="rounded-lg px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing[3])-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] text-base/6 placeholder:text-zinc-500 sm:text-sm/6 text-white border bg-blue-500 border-blue-600 data-[hover]:border-blue-700 bg-blue hover:bg-blue-700 focus:outline-none cursor-pointer">
+                Create Event
+              </button>
+            </Link>
+          </div>
         </div>
+        <span className="text-zinc-700 text-xs p-4 block text-right">
+          Scroll ↓
+        </span>
       </div>
 
       <div className="overflow-scroll max-h-full border-t border-zinc-800">
@@ -38,14 +43,14 @@ export default function EventsList({
             className="flex items-center justify-between p-4 border-b border-zinc-800 hover:bg-zinc-800 cursor-pointer"
             onClick={() => handleClick(event)}
           >
-            <div className="mb-12">
+            <div className="mb-8">
               <h2 className="text-white font-semibold mb-2">{event.title}</h2>
               <p className="text-gray-400 text-sm mb-8">{event.address}</p>
             </div>
 
-            <div>
+            <div className="mt-12">
               <EventTypeIcon type={event.eventType.type} />
-              <p className="text-gray-400 text-sm mt-12 mb-2">
+              <p className="text-gray-400 text-sm  mt-2">
                 {new Date(event.createdAt).toLocaleString("en-US", {
                   month: "short",
                   year: "numeric",
