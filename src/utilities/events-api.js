4765e22ae14f5d6e82e76dbb122d0feec193e640
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-21 01:19:50 -0500

    removed boundaries

--- src/utilities/events-api.js
+++ src/utilities/events-api.js
@@ -1,8 +1,8 @@
 import sendRequest from "./send-request";
 const BASE_URL = "/api/events";
 
-export async function getAllEvents(boundaries) {
-  return sendRequest(`${BASE_URL}/`, boundaries);
+export async function getAllEvents() {
+  return sendRequest(`${BASE_URL}/`);
 }
 
 export async function getOneEvent(id) {
