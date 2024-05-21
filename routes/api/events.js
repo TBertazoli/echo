5ef0f72ec71fa4876b9540d12cf0ae4433566b06
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-21 18:35:39 -0500

    created route and controller for media upload

--- routes/api/events.js
+++ routes/api/events.js
@@ -8,5 +8,6 @@ router.get("/", eventsCtrl.show); //to show only user reports
 router.get("/:id", eventsCtrl.showOne); //to show only users report by id
 router.put("/:id", eventsCtrl.update); //
 router.delete("/:id", eventsCtrl.delete);
+router.post("/:id/addMedia", eventsCtrl.addMedia);
 
 module.exports = router;
