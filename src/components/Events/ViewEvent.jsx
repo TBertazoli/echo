5ea654a10dc74ead0b589c9da250e0867362285c
miguelcoria94 <miguelcoria94@gmail.com>
2024-05-22 12:07:31 -0500

    improved delete ui

--- src/components/Events/ViewEvent.jsx
+++ src/components/Events/ViewEvent.jsx
@@ -9,12 +9,14 @@ import EventTypeIcon from "./EventTypeIcon";
 import Modal from "react-modal";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { set } from "mongoose";
 
 export default function ViewEvent({ user }) {
   const { id } = useParams();
   const [event, setEvent] = useState(null);
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const history = useNavigate();
+  const [isLoadingDelete, setIsLoading] = useState(false);
 
   function openModal() {
     setIsOpen(true);
@@ -49,11 +51,12 @@ export default function ViewEvent({ user }) {
   }, [id]);
 
   async function handleDelete() {
+    setIsLoading(true);
     try {
       await UserEventsService.deleteUserEvent(id);
       toast.info("Waiting for event to be deleted...", {
         position: "top-center",
-        autoClose: 2000,
+        autoClose: 1000,
         theme: "dark",
         icon: false,
         onClose: () => {
@@ -62,6 +65,7 @@ export default function ViewEvent({ user }) {
       });
     } catch {
       console.log("Error deleting event");
+    } finally {
     }
   }
   const handleCopyUrl = () => {
@@ -144,7 +148,29 @@ export default function ViewEvent({ user }) {
             onClick={handleDelete}
             className="bg-red-500 text-white px-4 py-2 rounded-lg"
           >
-            Yes
+            {isLoadingDelete ? (
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-red-400"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            ) : (
+              "Yes"
+            )}
           </button>
           <button
             onClick={closeModal}
