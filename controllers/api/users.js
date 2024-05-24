fabe27aa18867cfe3c3835f0664eb005bf33358d
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-05-23 23:22:44 -0500

    fixed error and cleanup console.logs

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,15 +1,14 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const User = require('../../models/user');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../../models/user");
 
 module.exports = {
   create,
   login,
-  checkToken
+  checkToken,
 };
 
 function checkToken(req, res) {
-  console.log('req.user', req.user);
   res.json(req.exp);
 }
 
@@ -26,14 +25,14 @@ async function create(req, res) {
 
 async function login(req, res) {
   try {
-    const user = await User.findOne({email: req.body.email});
+    const user = await User.findOne({ email: req.body.email });
     if (!user) throw new Error();
     const match = await bcrypt.compare(req.body.password, user.password);
     if (!match) throw new Error();
     const token = createJWT(user);
     res.json(token);
   } catch (err) {
-    res.status(400).json('Bad Credentials');
+    res.status(400).json("Bad Credentials");
   }
 }
 
@@ -44,6 +43,6 @@ function createJWT(user) {
     // data payload
     { user },
     process.env.SECRET,
-    { expiresIn: '24h' }
+    { expiresIn: "24h" }
   );
 }
